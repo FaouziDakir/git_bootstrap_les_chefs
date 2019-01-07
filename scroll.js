deb702675288307a59cb781f41aea718445becc8
Faouzi <l.a.-noire@hotmail.com>
2019-01-07 16:45:26 +0100

    fin site version 1.2

---
+++ scroll.js
@@ -0,0 +1,17 @@
+window.onscroll = function(){
+
+    if(document.documentElement.scrollTop > 800){
+        document.getElementById('nav1').classList.add('bg-dark');
+        document.getElementById('nav2').classList.add('bg-dark');
+        document.getElementById('nav3').classList.add('d-none');
+
+    } else {
+        document.getElementById('nav1').classList.remove('bg-dark');
+        document.getElementById('nav2').classList.remove('bg-dark');
+        document.getElementById('nav3').classList.remove('d-none');
+
+
+    }
+};
+
+// scrollEventActive(0, 600, 'l-home');
